c84e33af17e4d6e0389cf2f33000910c01ae80f6
chiomzz <chiomaikerionwu@gmail.com>
2025-07-24 21:13:21 -0600

    Add customer details

---
+++ Add_Customer.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://automationplayground.com/crm/login.html")
+driver.maximize_window()
+
+#login
+enter_Email_address = driver.find_element(By.ID, "email-id")
+enter_Email_address.send_keys("chike@yahoo.com")
+enter_Password = driver.find_element(By.CSS_SELECTOR, "#password")
+enter_Password.send_keys("1234")
+click_submit_button = driver.find_element(By.ID, "submit-id")
+click_submit_button.click()
+time.sleep(5)
+
+#Click on New Customer Button
+click_NewCustomer_button = driver.find_element(By.ID, 'new-customer')
+click_NewCustomer_button.click()
+
+#Add Customer Details
+enter_email_address = driver.find_element(By.CSS_SELECTOR, '#EmailAddress')
+enter_email_address.send_keys('chike@yahoo.com')
+enter_firstname = driver.find_element(By.CSS_SELECTOR, '#FirstName')
+enter_firstname.send_keys('Chy')
+enter_city = driver.find_element(By.XPATH, '//*[@id="City"]')
+enter_city.send_keys('Houston')
+click_state_button = driver.find_element(By.CSS_SELECTOR, '#StateOrRegion')
+click_state_button.click()
+#select_state = driver.find_element(By.XPATH, '')
+#select_state.click()
+click_gender_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[6]/input[2]')
+click_gender_button.click()
+check_add_to_promotional_list_box = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[7]/input')
+check_add_to_promotional_list_box.click()
+click_submit_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/button')
+click_submit_button.click()
+time.sleep(3)
