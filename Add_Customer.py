73ae32d0fbc5a3f1548931afab0d2a90160c645c
chiomzz <chiomaikerionwu@gmail.com>
2025-07-27 19:34:24 -0600

    Add Customer Form

--- Add_Customer.py
+++ Add_Customer.py
@@ -23,18 +23,35 @@ click_NewCustomer_button.click()
 #Add Customer Details
 enter_email_address = driver.find_element(By.CSS_SELECTOR, '#EmailAddress')
 enter_email_address.send_keys('chike@yahoo.com')
+
 enter_firstname = driver.find_element(By.CSS_SELECTOR, '#FirstName')
 enter_firstname.send_keys('Chy')
+
+enter_Lastname = driver.find_element(By.ID,"LastName")
+enter_Lastname.send_keys("Oma")
+
 enter_city = driver.find_element(By.XPATH, '//*[@id="City"]')
 enter_city.send_keys('Houston')
+
 click_state_button = driver.find_element(By.CSS_SELECTOR, '#StateOrRegion')
 click_state_button.click()
-#select_state = driver.find_element(By.XPATH, '')
-#select_state.click()
+
+select_state = driver.find_element(By.XPATH, '//*[@id="StateOrRegion"]/option[46]')
+select_state.click()
+
 click_gender_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[6]/input[2]')
 click_gender_button.click()
-check_add_to_promotional_list_box = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/div[7]/input')
-check_add_to_promotional_list_box.click()
+
+click_promotional_list_button = driver.find_element(By.CSS_SELECTOR, '#loginform > div > div > div > div > form > div:nth-child(7) > input[type=checkbox]')
+click_promotional_list_button.click()
+
 click_submit_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div/div/div/div/form/button')
 click_submit_button.click()
+
+time.sleep(3)
+
+#SignOut
+click_sign_out_button = driver.find_element(By.CSS_SELECTOR,"body > nav > ul > li > a")
+click_sign_out_button.click()
+
 time.sleep(3)
